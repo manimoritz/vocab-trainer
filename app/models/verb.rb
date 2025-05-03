c767bd71c9abbde5aa410e4b2fcb5dbdfb5f9452
Moritz <manimoritz@gmail.com>
2025-05-03 14:07:25 +0200

    Restructure verb class and correct behavior so that the tests pass

--- app/models/verb.rb
+++ app/models/verb.rb
@@ -6,23 +6,103 @@ class Verb < ApplicationRecord
   validates :perfect_active, presence: true
   validates :supine, presence: true
 
+  ENDINGS = {
+    indicative: {
+      present: {
+        active: {
+          1 => %w[õ ãs at ãmus ãtis ant],   # First conjugation
+          2 => %w[eõ ẽs et ẽmus ẽtis ent],  # Second conjugation
+          3 => %w[õ is it imus itis unt],   # Third conjugation
+          3.5 => %w[iõ is it imus itis iunt], # Third -iō conjugation
+          4 => %w[iõ ĩs it ĩmus ĩtis iunt] # Fourth conjugation
+        },
+        passive: {
+          1 => %w[or ãris ãtur ãmur ãminĩ antur],
+          2 => %w[eor ẽris ẽtur ẽmur ẽminĩ entur],
+          3 => %w[or eris itur imur iminĩ untur],
+          3.5 => %w[ior eris itur imur iminĩ iuntur],
+          4 => %w[ior ĩris ĩtur ĩmur ĩminĩ iuntur]
+        }
+      },
+      future: {
+        active: {
+          1 => %w[bõ bis bit bimus bitis bunt],
+          2 => %w[bõ bis bit bimus bitis bunt],
+          3 => %w[am ẽs et ẽmus ẽtis ent],
+          3.5 => %w[iam iẽs iet iẽmus iẽtis ient],
+          4 => %w[iam iẽs iet iẽmus iẽtis ient]
+        },
+        passive: {
+          1 => %w[bor beris bitur bimur biminĩ buntur],
+          2 => %w[bor beris bitur bimur biminĩ buntur],
+          3 => %w[ar ẽris ẽtur ẽmur ẽminĩ entur],
+          3.5 => %w[ar ẽris ẽtur ẽmur ẽminĩ entur],
+          4 => %w[ar ẽris ẽtur ẽmur ẽminĩ entur]
+        }
+      },
+      imperfect: {
+        active: Hash.new(%w[bam bãs bat bãmus bãtis bant]),
+        passive: Hash.new(%w[bar bãris bãtur bãmur bãminĩ bantur])
+      },
+      perfect: {
+        active: Hash.new(%w[ĩ istĩ it imus istis ẽrunt]),
+        passive: Hash.new([' sum', ' es', ' est', ' sumus', ' estis', ' sunt'])
+      },
+      future_perfect: {
+        active: Hash.new(%w[erõ eris erit erimus eritis erint]),
+        passive: Hash.new([' erõ', ' eris', ' erit', ' erimus', ' eritis', ' erunt'])
+      },
+      pluperfect: {
+        active: Hash.new(%w[eram erãs erat erãmus erãtis erant]),
+        passive: Hash.new([' eram', ' erãs', ' erat', ' erãmus', ' erãtis', ' erant'])
+      }
+    },
+    subjunctive: {
+      present: {
+        active: Hash.new(%w[am ãs at ãmus ãtis ant]).merge({ 1 => %w[em ẽs et ẽmus ẽtis ent] }),
+        passive: Hash.new(%w[ar ãris ãtur ãmur ãminĩ antur]).merge({ 1 => %w[er ẽris ẽtur ẽmur ẽminĩ entur] })
+      },
+      imperfect: {
+        active: Hash.new(%w[em ẽs et ẽmus ẽtis ent]),
+        passive: Hash.new(%w[er ẽris ẽtur ẽmur ẽminĩ entur])
+      },
+      perfect: {
+        active: Hash.new(%w[erim erĩs erit erĩmus erĩtis erint]),
+        passive: Hash.new([' sim', ' sĩs', ' sit', ' sĩmus', ' sĩtis', ' sint'])
+      },
+      pluperfect: {
+        active: Hash.new(%w[ssem ssẽs sset ssẽmus ssẽtis ssent]),
+        passive: Hash.new([' essem', ' essẽs', ' esset', ' essẽmus', ' essẽtis', ' essent'])
+      }
+    }
+  }.freeze
+
   def self.not_learned_yet
     # TODO: Replace this placeholder logic with some learning system similar to Anki.
     Verb.all
   end
 
   def conjugation_list
-    # Establish logic for the distinction between conjugations.
-    forms_for_conjugation('1', 'present', 'active', 'indicative')
+    forms_for_conjugation(conjugation, 'present', 'active', 'indicative')
   end
 
   private
+
+  def conjugation
+    return 1 if present_infinitive[-3] == 'ã'
+    return 2 if present_infinitive[-3] == 'ẽ'
+    return 3.5 if present_infinitive[-3] == 'e' && present_active[-2..] == 'iõ'
+    return 3 if present_infinitive[-3] == 'e'
+    return 4 if present_infinitive[-3] == 'ĩ'
+
+    raise "'#{present_infinitive}' does not fit any conjugation rule."
+  end
 
   def forms_for_conjugation(conjugation, tense, voice, mood)
     persons = %i[first_person second_person third_person]
     numbers = %i[singular singular singular plural plural plural]
 
-    selected_endings = endings[mood.to_sym][tense.to_sym][voice.to_sym][conjugation.to_i] || []
+    selected_endings = ENDINGS[mood.to_sym][tense.to_sym][voice.to_sym][conjugation] || []
 
     selected_endings.each_with_index.map do |ending, index|
       {
@@ -45,79 +125,6 @@ class Verb < ApplicationRecord
       perfect: perfect_active[..-2],
       future_perfect: perfect_active[..-2],
       pluperfect: perfect_active[..-2]
-    }
-  end
-
-  def endings
-    {
-      indicative: {
-        present: {
-          active: {
-            1 => %w[õ ãs at ãmus ãtis ant],   # First conjugation
-            2 => %w[eõ ẽs et ẽmus ẽtis ent],  # Second conjugation
-            3 => %w[õ is it imus itis unt],   # Third conjugation
-            3.5 => %w[iõ is it imus itis iunt], # Third -iō conjugation
-            4 => %w[iõ ĩs it ĩmus ĩtis iunt] # Fourth conjugation
-          },
-          passive: {
-            1 => %w[or ãris ãtur ãmur ãminĩ antur],
-            2 => %w[eor ẽris ẽtur ẽmur ẽminĩ entur],
-            3 => %w[or eris itur imur iminĩ untur],
-            3.5 => %w[ior eris itur imur iminĩ iuntur],
-            4 => %w[ior ĩris ĩtur ĩmur ĩminĩ iuntur]
-          }
-        },
-        future: {
-          active: {
-            1 => %w[bõ bis bit bimus bitis bunt],
-            2 => %w[bõ bis bit bimus bitis bunt],
-            3 => %w[am ẽs et ẽmus ẽtis ent],
-            3.5 => %w[iam iẽs iet iẽmus iẽtis ient],
-            4 => %w[iam iẽs iet iẽmus iẽtis ient]
-          },
-          passive: {
-            1 => %w[bor beris bitur bimur biminĩ buntur],
-            2 => %w[bor beris bitur bimur biminĩ buntur],
-            3 => %w[ar ẽris ẽtur ẽmur ẽminĩ entur],
-            3.5 => %w[ar ẽris ẽtur ẽmur ẽminĩ entur],
-            4 => %w[ar ẽris ẽtur ẽmur ẽminĩ entur]
-          }
-        },
-        imperfect: {
-          active: Hash.new(%w[bam bãs bat bãmus bãtis bant]),
-          passive: Hash.new(%w[bar bãris bãtur bãmur bãminĩ bantur])
-        },
-        perfect: {
-          active: Hash.new(%w[ĩ istĩ it imus istis ẽrunt]),
-          passive: Hash.new([' sum', ' es', ' est', ' sumus', ' estis', ' sunt'])
-        },
-        future_perfect: {
-          active: Hash.new(%w[erõ eris erit erimus eritis erint]),
-          passive: Hash.new([' erõ', ' eris', ' erit', ' erimus', ' eritis', ' erunt'])
-        },
-        pluperfect: {
-          active: Hash.new(%w[eram erãs erat erãmus erãtis erant]),
-          passive: Hash.new([' eram', ' erãs', ' erat', ' erãmus', ' erãtis', ' erant'])
-        }
-      },
-      subjunctive: {
-        present: {
-          active: Hash.new(%w[am ãs at ãmus ãtis ant]).merge({ 1 => %w[em ẽs et ẽmus ẽtis ent] }),
-          passive: Hash.new(%w[ar ãris ãtur ãmur ãminĩ antur]).merge({ 1 => %w[er ẽris ẽtur ẽmur ẽminĩ entur] })
-        },
-        imperfect: {
-          active: Hash.new(%w[em ẽs et ẽmus ẽtis ent]),
-          passive: Hash.new(%w[er ẽris ẽtur ẽmur ẽminĩ entur])
-        },
-        perfect: {
-          active: Hash.new(%w[erim erĩs erit erĩmus erĩtis erint]),
-          passive: Hash.new([' sim', ' sĩs', ' sit', ' sĩmus', ' sĩtis', ' sint'])
-        },
-        pluperfect: {
-          active: Hash.new(%w[ssem ssẽs sset ssẽmus ssẽtis ssent]),
-          passive: Hash.new([' essem', ' essẽs', ' esset', ' essẽmus', ' essẽtis', ' essent'])
-        }
-      }
     }
   end
 end
